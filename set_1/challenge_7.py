16423ec703258e73714fcd50e875bae6caa0d3b1
bitsandsalsa <bitsandsalsa@users.noreply.github.com>
2017-12-28 23:10:14 -0800

    disallow cipher block mode selection

--- set_1/challenge_7.py
+++ set_1/challenge_7.py
@@ -15,18 +15,18 @@ OUT_PLAINTEXT_LINE78 = "Play that funky music "  # last line before padding
 
 logger = logging.getLogger(__name__)
 
-def aes_encrypt(data, key, mode, iv=''):
-    cipher = AES.new(key, mode=mode, IV=iv)
+def aes_ecb_encrypt(data, key):
+    cipher = AES.new(key, mode=AES.MODE_ECB)
     return cipher.encrypt(data)
 
-def aes_decrypt(data, key, mode, iv=''):
-    cipher = AES.new(key, mode=mode, IV=iv)
+def aes_ecb_decrypt(data, key):
+    cipher = AES.new(key, mode=AES.MODE_ECB)
     return cipher.decrypt(data)
 
 def run_tests():
     ciphertext = open(IN_CIPHERTEXT_FILE).read().decode('base64')
     logger.info('Decrypting data using AES in ECB mode.')
-    plaintext = aes_decrypt(ciphertext, IN_KEY, AES.MODE_ECB)
+    plaintext = aes_ecb_decrypt(ciphertext, IN_KEY)
     logger.debug('Decrypted data:\n-----\n%s\n-----', plaintext)
 
     plaintext_lines = plaintext.split('\n')
@@ -34,7 +34,7 @@ def run_tests():
     assert OUT_PLAINTEXT_LINE78 == plaintext_lines[78], 'Failed to decrypt line 78.'
 
     logger.info('Re-encrypting the plaintext using AES in ECB mode.')
-    test_ct = aes_encrypt(plaintext, IN_KEY, AES.MODE_ECB)
+    test_ct = aes_ecb_encrypt(plaintext, IN_KEY)
     assert ciphertext == test_ct, 'Failed to re-encrypt decrypted data.'
 
 if __name__ == '__main__':
